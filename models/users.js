5662910ace6dd98d8594786cf11df812fbb566ab
Ricardo Esparza <ricardo.fanizzi@gmail.com>
2019-11-27 17:44:09 +0100

    funciones y get usuarios

--- models/users.js
+++ models/users.js
@@ -1,15 +1,65 @@
 const moment = require('moment');
-
-const getAll = ()=>{
+/* Obtener todos los usuarios */
+const getAll = () => {
     return new Promise((resolve, reject)=>{
         db.query('SELECT * FROM users', (err, rows)=>{
             if(err) reject(err)
-            resolve(rows)
+            rows.map(row => {
+                row = dateFormatAll(row)
+            })
+            resolve(rows);
         });
-    })
+    });
+};
+
+/* Obtener todos los usuarios activos o inactivos */
+const getAllActive = (pActive) => {
+    return new Promise((resolve, reject) =>{
+        db.query('SELECT * FROM users WHERE isActive = ?', [pActive], (err, rows)=>{
+            if(err) reject(err)
+            rows.map(row => {
+                row = dateFormatAll(row)
+            })
+            resolve(rows);
+        });
+    });
+};
+
+
+/* Obtener usuarios por su ID */
+const getById = (pId) => {
+    return new Promise((resolve, reject)=>{
+        db.query('SELECT * FROM users WHERE id = ?', [pId], (err, rows) => {
+            if(err) reject(err)
+            rows[0] = dateFormatAll(rows[0])
+            resolve(rows[0])
+        });
+    });
+};
+
+
+
+
+/* Funciones */
+
+//Formato por fecha para datos
+let dateFormat = (pDate, pOptionalFormat = 'DD-MM-YYYY') => {
+   return pDate = (pDate != null) ? moment(pDate).format(pOptionalFormat): ''
 }
 
+//Formato por fecha para objetos
+let dateFormatAll = (pRow) =>{
+    pRow.endAt = dateFormat(pRow.endAt);
+    pRow.startAt = dateFormat(pRow.startAt);
+    pRow.eulaDate = dateFormat(pRow.eulaDate);
+    pRow.updateAt = dateFormat(pRow.updateAt ,'DD-MM-YYYY HH:MM:ss') 
+    return pRow
+}
+
+/* Final de funciones */
 
 module.exports = {
-    getAll : getAll
+    getAll : getAll,
+    getById : getById,
+    getAllActive: getAllActive
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-const Users = require('../../models/users')
-router.get('/', (req, res)=>{
+const Users = require('../../models/users');
+
+/* Ruta principal de acceso a todos los usuarios */
+router.get('/', (req, res) => {
     Users.getAll()
     .then(rows=>{
-        res.json(rows)
+        res.json(rows);
+    })
+    .catch(err => console.log(err));
+});
+
+/* Ruta de acceso a todos los usuarios activos */
+router.get('/active', (req, res) => {
+    Users.getAllActive(true)
+    .then(rows=>{
+        res.json(rows);
+    })
+    .catch(err => console.log(err));
+});
+
+/* Ruta de acceso a usuario por ID */
+router.get('/:idUsuario', (req, res) => {
+    Users.getById(req.params.idUsuario)
+    .then(row => {
+        res.json(row)
     })
     .catch(err => console.log(err))
-})
-
+});
 module.exports = router;
