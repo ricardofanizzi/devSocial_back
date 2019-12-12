c9b781e80161c51dfe7903cbd33d3418190db9c4
Cristian Salerno <cristiansalerno18@gmail.com>
2019-12-12 17:02:32 +0100

    caja de comentarios API

--- models/users.js
+++ models/users.js
@@ -1,124 +1,150 @@
-const moment = require('moment');
+const moment = require("moment");
 
 /* Obtener todos los usuarios */
 const getAll = () => {
-    return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM users', (err, rows) => {
-            if (err) reject(err)
-            rows.map(row => {
-                row = dateFormatAll(row)
-            })
-            resolve(rows);
-        });
+  return new Promise((resolve, reject) => {
+    db.query("SELECT * FROM users", (err, rows) => {
+      if (err) reject(err);
+      rows.map(row => {
+        row = dateFormatAll(row);
+      });
+      resolve(rows);
     });
+  });
 };
 
 /* Obtener todos los usuarios activos o inactivos */
-const getAllActive = (pActive) => {
-    return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM users WHERE isActive = ?', [pActive], (err, rows) => {
-            if (err) reject(err)
-            rows.map(row => {
-                row = dateFormatAll(row)
-            })
-            resolve(rows);
+const getAllActive = pActive => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      "SELECT * FROM users WHERE isActive = ?",
+      [pActive],
+      (err, rows) => {
+        if (err) reject(err);
+        rows.map(row => {
+          row = dateFormatAll(row);
         });
-    });
+        resolve(rows);
+      }
+    );
+  });
 };
 
-
-
 /* Obtener usuarios por su ID */
-const getById = (pId) => {
-    return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM users WHERE id = ?', [pId], (err, rows) => {
-            if (err) reject(err)
-            rows[0] = dateFormatAll(rows[0])
-            resolve(rows[0])
-        });
+const getById = pId => {
+  return new Promise((resolve, reject) => {
+    db.query("SELECT * FROM users WHERE id = ?", [pId], (err, rows) => {
+      if (err) reject(err);
+      rows[0] = dateFormatAll(rows[0]);
+      resolve(rows[0]);
     });
+  });
 };
 
 /* Obtener usuarios por su Email */
-const getByEmail = (pEmail) => {
-    return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM users WHERE email = ?', [pEmail], (err, rows) => {
-            if (err) reject(err)
-            if (rows[0]) rows[0] = dateFormatAll(rows[0])
-            resolve(rows[0])
-        });
+const getByEmail = pEmail => {
+  return new Promise((resolve, reject) => {
+    db.query("SELECT * FROM users WHERE email = ?", [pEmail], (err, rows) => {
+      console.log(rows);
+      if (err) reject(err);
+      if (rows[0]) rows[0] = dateFormatAll(rows[0]);
+      resolve(rows[0]);
     });
+  });
 };
 
 /* Registro de usuarios */
 const insert = ({ email, username, password, name, surnames, imageUrl }) => {
-    return new Promise((resolve, reject) => {
-        db.query('INSERT INTO users ( email, username, password, name, surnames, imageUrl, isActive, startAt, updateAt) VALUES (?, ?, ?, ? ,? ,?, ?, ?, ? )', [email, username, password, name, surnames, imageUrl, true, new Date(), new Date()], (err, result) => {
-            if (err) reject(err)
-            if (result) {
-                resolve(result)
-            };
-        });
-    });
+  return new Promise((resolve, reject) => {
+    db.query(
+      "INSERT INTO users ( email, username, password, name, surnames, imageUrl, isActive, startAt, updateAt) VALUES (?, ?, ?, ? ,? ,?, ?, ?, ? )",
+      [
+        email,
+        username,
+        password,
+        name,
+        surnames,
+        imageUrl,
+        true,
+        new Date(),
+        new Date()
+      ],
+      (err, result) => {
+        if (err) reject(err);
+        if (result) {
+          resolve(result);
+        }
+      }
+    );
+  });
 };
-
-
-
 
 /* Actualización de usuarios */
 const updateNames = ({ id, username, name, surnames }) => {
-    return new Promise((resolve, reject) => {
-        db.query('UPDATE users SET username = ?, name = ?, surnames = ? WHERE id = ?', [username, name, surnames, id], (err, result) => {
-            if (err) reject(err)
-            resolve(result)
-        });
-    });
+  return new Promise((resolve, reject) => {
+    db.query(
+      "UPDATE users SET username = ?, name = ?, surnames = ? WHERE id = ?",
+      [username, name, surnames, id],
+      (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      }
+    );
+  });
 };
 
 const updateEmail = ({ id, email }) => {
-    return new Promise((resolve, reject) => {
-        db.query('UPDATE users SET email = ? WHERE id = ?', [email, id], (err, result) => {
-            if (err) reject(err)
-            resolve(result)
-        });
-    });
+  return new Promise((resolve, reject) => {
+    db.query(
+      "UPDATE users SET email = ? WHERE id = ?",
+      [email, id],
+      (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      }
+    );
+  });
 };
 
 const updatePassword = ({ id, password }) => {
-    return new Promise((resolve, reject) => {
-        db.query('UPDATE users  SET password = ? WHERE id = ?', [password, id], (err, result) => {
-            if (err) reject(err)
-            resolve(result)
-        });
-    });
+  return new Promise((resolve, reject) => {
+    db.query(
+      "UPDATE users  SET password = ? WHERE id = ?",
+      [password, id],
+      (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      }
+    );
+  });
 };
 
 /* Funciones */
 
 //Formato por fecha para datos
-let dateFormat = (pDate, pOptionalFormat = 'DD-MM-YYYY') => {
-    return pDate = (pDate != null) ? moment(pDate).format(pOptionalFormat) : ''
-}
+let dateFormat = (pDate, pOptionalFormat = "DD-MM-YYYY") => {
+  return (pDate = pDate != null ? moment(pDate).format(pOptionalFormat) : "");
+};
 
 //Formato por fecha para objetos
-let dateFormatAll = (pRow) => {
-    console.log(pRow)
-    pRow.endAt = dateFormat(pRow.endAt);
-    pRow.startAt = dateFormat(pRow.startAt);
-    pRow.eulaDate = dateFormat(pRow.eulaDate);
-    pRow.updateAt = dateFormat(pRow.updateAt, 'DD-MM-YYYY HH:MM:ss')
-    return pRow
-}
+let dateFormatAll = pRow => {
+  console.log(pRow);
+  pRow.endAt = dateFormat(pRow.endAt);
+  pRow.startAt = dateFormat(pRow.startAt);
+  pRow.eulaDate = dateFormat(pRow.eulaDate);
+  pRow.updateAt = dateFormat(pRow.updateAt, "DD-MM-YYYY HH:MM:ss");
+  return pRow;
+};
 
 /* Final de funciones */
 
 module.exports = {
-    getAll: getAll,
-    getById: getById,
-    getByEmail: getByEmail,
-    getAllActive: getAllActive,
-    insert: insert,
-    updateNames: updateNames,
-    updateEmail: updateEmail,
-    updatePassword: updatePassword
-}
+  getAll: getAll,
+  getById: getById,
+  getByEmail: getByEmail,
+  getAllActive: getAllActive,
+  insert: insert,
+  updateNames: updateNames,
+  updateEmail: updateEmail,
+  updatePassword: updatePassword
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,128 +1,129 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jwt-simple');
-const moment = require('moment');
-const middlewares = require('../middleware')
-const Users = require('../../models/users');
+const bcrypt = require("bcryptjs");
+const jwt = require("jwt-simple");
+const moment = require("moment");
+const middlewares = require("../middleware");
+const Users = require("../../models/users");
 
-//GETS  
+//GETS
 /* Ruta principal de acceso a todos los usuarios */
 
 //POSTS
 /* Ruta para registro de usuario */
-router.post('/register', async (req, res) => {
-    try {
-        req.body.password = bcrypt.hashSync(req.body.password, 10);
-        const result = await Users.insert(req.body);
-        res.json(result);
-        console.log(req.body);
-    }
-    catch (err) {
-        console.log(err)
-    }
+router.post("/register", async (req, res) => {
+  try {
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
+    const result = await Users.insert(req.body);
+    res.json(result);
+    console.log(req.body);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.post('/login', async (req, res) => {
-    const user = await Users.getByEmail(req.body.email)
-    if (user === undefined) {
-        res.json({
-            error: 'Error, email or password not found'
-        })
+router.post("/login", async (req, res) => {
+  const user = await Users.getByEmail(req.body.email);
+  console.log(user);
+  if (user === undefined) {
+    res.json({
+      error: "Error, email or password not found"
+    });
+  } else {
+    const equals = bcrypt.compare(req.body.password, user.password);
+    if (!equals) {
+      res.json({
+        error: "Error, email or password not found"
+      });
     } else {
-        const equals = bcrypt.compare(req.body.password, user.password);
-        if (!equals) {
-            res.json({
-                error: 'Error, email or password not found'
-            })
-        } else {
-            res.json({
-                succesfull: createToken(user),
-                done: 'Login correct'
-            });
-        }
+      res.json({
+        succesfull: createToken(user),
+        done: "Login correct"
+      });
     }
+  }
 });
 
-router.use(middlewares.checkToken)
+router.use(middlewares.checkToken);
 
-
-router.get('/main', (req, res) => {
-    console.log(req.userId);
-    Users.getById(req.userId)
-        .then(rows => {
-            console.log(rows)
-            res.json(rows);
-        })
-        .catch(err => console.log(err));
+router.get("/main", (req, res) => {
+  console.log(req.userId);
+  Users.getById(req.userId)
+    .then(rows => {
+      console.log(rows);
+      res.json(rows);
+    })
+    .catch(err => console.log(err));
 });
 
-router.get('/', (req, res) => {
-    Users.getAll()
-        .then(rows => {
-            res.json(rows);
-        })
-        .catch(err => console.log(err));
+router.get("/", (req, res) => {
+  Users.getAll()
+    .then(rows => {
+      res.json(rows);
+    })
+    .catch(err => console.log(err));
 });
 
 /* Ruta de acceso a todos los usuarios activos */
-router.get('/active', (req, res) => {
-    Users.getAllActive(true)
-        .then(rows => {
-            res.json(rows);
-        })
-        .catch(err => console.log(err));
+router.get("/active", (req, res) => {
+  Users.getAllActive(true)
+    .then(rows => {
+      res.json(rows);
+    })
+    .catch(err => console.log(err));
 });
 
 /* Ruta de acceso a usuario por ID */
-router.get('/:idUsuario', (req, res) => {
-    console.log(req.userId);
-    Users.getById(req.params.idUsuario)
-        .then(row => {
-            res.json(row)
-        })
-        .catch(err => console.log(err))
+router.get("/:idUsuario", (req, res) => {
+  console.log(req.userId);
+  Users.getById(req.params.idUsuario)
+    .then(row => {
+      res.json(row);
+    })
+    .catch(err => console.log(err));
 });
 
-router.post('/updatenames', async (req, res) => {
-    req.body.id = req.userId
-    const result = await Users.updateNames(req.body);
+router.post("/updatenames", async (req, res) => {
+  req.body.id = req.userId;
+  const result = await Users.updateNames(req.body);
+  console.log(result);
+  res.json(result);
+});
+
+router.post("/updatemail", async (req, res) => {
+  req.body.id = req.userId;
+  const result = await Users.updateEmail(req.body);
+  console.log(result);
+  res.json(result);
+});
+
+router.post("/updatepass", async (req, res) => {
+  const user = await Users.getById(req.userId);
+  req.body.id = req.userId;
+  const equals = bcrypt.compare(req.body.oldPassword, user.password);
+  if (!equals) {
+    res.json({
+      error: "Old password is incorrect"
+    });
+  } else {
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
+    const result = await Users.updatePassword(req.body);
     console.log(result);
-    res.json(result);
+    res.json({
+      succesfull: "Update succesfull"
+    });
+  }
 });
 
-router.post('/updatemail', async (req, res) => {
-    req.body.id = req.userId;
-    const result = await Users.updateEmail(req.body);
-    console.log(result);
-    res.json(result);
-});
-
-router.post('/updatepass', async (req, res) => {
-    const user = await Users.getById(req.userId)
-    req.body.id = req.userId;
-    const equals = bcrypt.compare(req.body.oldPassword, user.password)
-    if (!equals) {
-        res.json({
-            error: 'Old password is incorrect'
-        });
-    } else {
-        req.body.password = bcrypt.hashSync(req.body.password, 10);
-        const result = await Users.updatePassword(req.body);
-        console.log(result);
-        res.json({
-            succesfull: 'Update succesfull'
-        });
-    };
-});
-
-const createToken = (user) => {
-    let payload = {
-        userId: user.id,
-        createdAt: moment().unix(),
-        expiresAt: moment().add(1, 'day').unix()
-    }
-    return jwt.encode(payload, process.env.TOKEN_KEY);
+const createToken = user => {
+  let payload = {
+    userId: user.id,
+    createdAt: moment().unix(),
+    expiresAt: moment()
+      .add(1, "day")
+      .unix()
+  };
+  return jwt.encode(payload, process.env.TOKEN_KEY);
 };
 
 module.exports = router;
