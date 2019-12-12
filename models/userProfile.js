510e5b911cfb20b1d58874f65216953e5e833acc
Cristian Salerno <cristiansalerno18@gmail.com>
2019-12-12 16:34:54 +0100

    api de chat funcionando

--- models/userProfile.js
+++ models/userProfile.js
@@ -2,7 +2,7 @@
 const createUser = ({ speciality, availability, experience }) => {
   return new Promise((resolve, reject) => {
     db.query(
-      "INSERT INTO user_profile (speciality,availability,experience,about_me) VALUES (?,?,?)",
+      "INSERT INTO user_profile (speciality,availability,experience) VALUES (?,?,?)",
       [speciality, availability, experience],
       (error, result) => {
         if (error) reject(error);
