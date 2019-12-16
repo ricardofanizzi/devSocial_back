cf5a65c2d5aeaa99edb3508cbcfe894c389787ca
Cristian Salerno <cristiansalerno18@gmail.com>
2019-12-16 10:01:58 +0100

    api usertema query agregada

--- routes/api/userTema.js
+++ routes/api/userTema.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const UserTema = require('../../models/user_temas');
+const express = require("express");
+const UserTema = require("../../models/user_temas");
 
 const router = express.Router();
 
-
-
-router.post('/insert', async (req, res) => {
-    const result = await UserTema.insert(req.body);
-    res.json(result);
+router.post("/insert", async (req, res) => {
+  const result = await UserTema.insert(req.body);
+  res.json(result);
 });
 
+//GET http://localhost:3000/usertema/allData/:2
+router.get("/allData/:userId", async (req, res) => {
+  const rows = await UserTema.getTemaByUser(req.params.userId);
+  console.log(rows.json);
+});
 
-module.exports = router
+module.exports = router;
